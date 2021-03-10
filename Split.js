c4b6adaa40c5d1455bac0abdbfa45f2defb5c6b2
Akash Keshari <akash.keshari@gitcode.com>
2021-03-10 20:52:04 +0530

    Made readable for split() as well

--- Split.js
+++ Split.js
@@ -11,7 +11,7 @@ let anystr = 'A type of string';
 const noArgsPassed = anystr.split();
 console.log(noArgsPassed);
 
-// Separates the string on each character
+// ************************************************ Separates the string on each character ************************************************ */
 const charArray = anystr.split('');
 console.log(charArray);
 
@@ -21,11 +21,11 @@ console.log(charArray2);
 const charArray3 = Array.from(anystr);
 console.log(charArray3);
 
-// To Return a limited number of splits
+// ************************************************ To Return a limited number of splits ************************************************ */
 const limitedCharArray = anystr.split('', 3);
 console.log(limitedCharArray);
 
-// Separation done on space
+// ************************************************ Separation done on space ************************************************ */
 const spaceSeparatedStringArray = anystr.split(' ');
 console.log(spaceSeparatedStringArray);
 
@@ -34,13 +34,13 @@ const comma = ',';
 const arrayOfStrings = monthString.split(comma);
 console.log(arrayOfStrings);
 
-// Separation done based on Regular Expression
+// ************************************************ Separation done based on Regular Expression ************************************************ */
 const myString = 'Hello 1 word. Sentence number 2.';
 const regExp = /(\d)/;
 const splits = myString.split(regExp);
 console.log(splits);
 
-// Reversal of string
+// ************************************************ Reversal of string ************************************************ */
 const str = 'akash';
 const strReverse = str.split('').reverse().join('')
 console.log(strReverse);
