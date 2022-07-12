cf66d3703c47896e2cbb73f7a874e56fc86cdd03
Akash-ninja <akash.gitcode@gmail.com>
2022-07-12 20:49:33 +0530

    Renaming files and restructuring folders

--- JS-Fundamentals/JavaScript.js
+++ JS-Fundamentals/JavaScript.js
@@ -11,4 +11,4 @@
     // Special primitive type having additional usage for it's value: if object is not inherited, then null is shown;
 
     // Object : typeof instance === "object". Special non-data but structural
-    // type for any constructed object instance also used as data structures: new Object, new Array, new Map, new Set, new WeakMap, new WeakSet, new Date and almost everything made with new keyword;
+    // type for any constructed object instance also used as data structures: new Object, new Array, new Map, new Set, new WeakMap, new WeakSet, new Date and almost everything made with new keyword.
